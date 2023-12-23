612fc5d98468f42933337e7f32da3068ad7bcd88
GrandguyJS <GrandguyMC@gmail.com>
2023-12-23 21:59:19 +0100

    Restrucure + Login/Signup

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
-from flask import Flask
-from views import views
+from website import create_app
 
+app = create_app()
 
-
-
-
-app = Flask(__name__)
-
-
-app.register_blueprint(views)
-
-if __name__ == "__main__":
-
+if __name__ == '__main__':
     app.run(host = "0.0.0.0", port="8000", debug="True", ssl_context="adhoc")
+
--- static/main.js
+++
@@ -1,6 +0,0 @@
-/* Success Page */
-
-function copyURL() {
-    var copyText = document.getElementById("copyText");
-    navigator.clipboard.writeText(copyText.innerText);
- }
