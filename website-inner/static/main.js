f6c639e8189ecbdc71009db54b8957e089d3b5b5
GrandguyJS <GrandguyMC@gmail.com>
2023-12-24 12:01:11 +0100

    renaming website-inner -> websiteroute

--- website-inner/static/main.js
+++
@@ -1,8 +0,0 @@
-/* Success Page */
-
-function copyURL(event) {
-    var div = event.currentTarget;
-    var child = div.children[2];
-    var copyText = child.innerText;
-    navigator.clipboard.writeText(copyText);
- }
