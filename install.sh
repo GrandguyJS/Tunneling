eda722c8d46668bbc1e7fa056d3fd8dd1caaa2c0
GrandguyJS <GrandguyMC@gmail.com>
2024-06-09 17:17:40 +0200

    .sh files

--- install.sh
+++ install.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-sudo ufw allow 7780
 python3 -m venv /venv
 
 source /venv/bin/activate
 pip install -r requirements.txt
 pip install gunicorn
+
+sudo ufw allow 8000
+
+script_dir=$(dirname "$(readlink -f "$0")")
+cd websiteroute
+
+gunicorn -b 0.0.0.0:8000 --chdir $script_dir main:app
+
 deactivate
-
-export PORT="$port"
-
-./create_service.sh
-
-systemctl start tunneling
