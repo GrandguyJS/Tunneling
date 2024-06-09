5982975632b164e1145f7e37276b9d226341d19b
GrandguyJS <GrandguyMC@gmail.com>
2024-06-09 15:36:46 +0200

    fix

--- websiteroute/__init__.py
+++ websiteroute/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import os
+from os import path
+from os.path import abspath, dirname
 from flask_login import LoginManager
 
 db = SQLAlchemy()
@@ -8,9 +9,7 @@ DB_NAME = "database.db"
 
 
 def create_app():
-    template_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'templates'))
-    static_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'static'))
-    app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
+    app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
