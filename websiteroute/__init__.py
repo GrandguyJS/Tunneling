29565e3198839624add7d763b6e9958c32a26383
GrandguyJS <GrandguyMC@gmail.com>
2024-06-09 15:32:42 +0200

    fix

--- websiteroute/__init__.py
+++ websiteroute/__init__.py
@@ -9,8 +9,9 @@ DB_NAME = "database.db"
 
 
 def create_app():
-    app = Flask(__name__)
-    app.root_path = abspath(dirname(__file__))
+    template_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'templates'))
+    static_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'static'))
+    app = Flask(__name__, template_dir=template_dir, static_dir=static_dir)
     app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
