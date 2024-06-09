c5953536575f454dc75d2c6fa1a2af62e3f9ac64
GrandguyJS <GrandguyMC@gmail.com>
2024-06-09 16:41:26 +0200

    .sh files

--- install.sh
+++ install.sh
@@ -5,12 +5,8 @@ python3 -m venv /venv
 source /venv/bin/activate
 pip install -r requirements.txt
 pip install gunicorn
-
-
-
-script_dir=$(dirname "$(readlink -f "$0")")
-cd websiteroute
-
-gunicorn -b 0.0.0.0:8000 --chdir $script_dir main:app
-
 deactivate
+
+create_service.sh
+
+systemctl start tunelling
