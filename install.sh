f8b9f347653b20965fc2adbc43710aacb95eff3a
GrandguyJS <GrandguyMC@gmail.com>
2024-06-09 16:32:12 +0200

    .sh files

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-
+sudo ufw allow 8000
 python3 -m venv /venv
 
 source /venv/bin/activate
 pip install -r requirements.txt
 pip install gunicorn
 
-sudo ufw allow 8000
+
 
 script_dir=$(dirname "$(readlink -f "$0")")
 cd websiteroute
--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 script_dir=$(dirname "$(readlink -f "$0")")
 source /venv/bin/activate
+
 cd websiteroute
 
 gunicorn -b 0.0.0.0:8000 --chdir $script_dir main:app
