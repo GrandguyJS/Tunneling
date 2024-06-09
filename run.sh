54d2687ecfb42fe88ede695505ed6c9c83c055fd
GrandguyJS <GrandguyMC@gmail.com>
2024-06-09 16:35:52 +0200

    .sh files

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
-script_dir=$(dirname "$(readlink -f "$0")")
 source /venv/bin/activate
 
+script_dir=$(dirname "$(readlink -f "$0")")
 cd websiteroute
 
 gunicorn -b 0.0.0.0:8000 --chdir $script_dir main:app
+
+deactivate
